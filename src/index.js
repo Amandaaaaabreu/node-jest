d21a5436699e99973e352e537ced85e10f5933c1
Amandaaaaabreu <amandaabreu045@gmail.com>
2025-06-25 10:17:50 -0300

    criando as rotas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import {routes} from './routes.js';
 
 const server = express();
+server.use(express.json()); // falando pro servidor que vou trabalhar com JSON
+server.use(routes); // usando as rotas que foram criadas no arquivo routes.json
+
 
 server.listen(5000, () =>{
     console.log('Servidor rodando na porta 5000')
---
+++ src/routes.js
@@ -0,0 +1,38 @@
+import {response, Router} from 'express'
+
+const routes = Router()
+
+const database= ['Amanda']
+
+routes.get('/users', (request, response ) =>{
+    return response.status(200).json(database);
+})
+
+
+routes.post('/users',(request, response) => {
+    const body = request.body;
+    console.log(body);
+    return response.json(body);
+    
+})
+
+
+
+// STATUS CODE
+//200: Sucesso 
+//201: Sucesos e criação de recurso
+//204: Sucesso e sem conteudo
+//400: Erro na requisição
+//401: Não autorizado
+//404: Não autorizado/ Não encontrado 
+//500: Erro interno do servidor 
+
+
+
+// metodos HTTP: 
+// GET: Buscar ou listar uma informação
+//POST: Criar uma informação nova
+//PUT/PATCH: Atualizar uma informação existente 
+//DELETE: Deletar uma informação
+
+export {routes}
